455e0588c1ddae6ad7645062f711e2facb524ff9
Soham <kayalsoham61@gmail.com>
2023-02-11 19:51:24 +0530

    Created route for verifying user after KYC

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const {getUsers,addUser} = require('../controller/user')
+const {getUsers,addUser, verifyUser} = require('../controller/user')
 
 router.route('/').get(getUsers)
 router.route('/add').post(addUser)
+router.route('/verify/:id').put(verifyUser)
 
 module.exports = router
