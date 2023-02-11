b01d03a8532f47348843e4f4f225ab272e783e7d
SarveshLimaye <sarveshlimaye2002@gmail.com>
2023-02-11 16:33:21 +0530

    backend setup for kyc done

---
+++ backend/models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+    },
+    image: {
+        type: String,
+        default: 'https://static.tutordirect.com/prod/media/images/user-avatar-placeholder.max-320x320.png',
+        required: false
+    },
+    pancard:{
+        type: String,
+        required: false
+    },
+    aadharcard:{
+        type: String,
+    },
+    address:{
+        type: String,
+    },
+    dmataccountnumber:{
+        type: Number,
+    },
+    bonds:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Bonds',
+        default: []
+    }]
+
+})
+
+module.exports = mongoose.model('User', userSchema)
