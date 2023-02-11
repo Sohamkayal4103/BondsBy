f2ab86eb9cbfa34ec407100359d562b5de8602ae
SarveshLimaye <sarveshlimaye2002@gmail.com>
2023-02-11 14:54:13 +0530

    add bonds to db + model updated

--- backend/controller/bonds.js
+++ backend/controller/bonds.js
@@ -1,9 +1,14 @@
 const Bonds = require('../models/bonds');
 const asyncHandler = require('express-async-handler');
+const bondjson = require('../bonds.json');
 
 const getBonds = async (req, res) => {
     res.status(200).json(await Bonds.find())
 }
 
+const pushbond = async (req, res) => {
+  await Bonds.create(bondjson);
+}
 
-module.exports = {getBonds};
+
+module.exports = {getBonds,pushbond};
