6a09f9ad8fcbeb6bfa06ca8f180e8ff3654843be
Soham <kayalsoham61@gmail.com>
2023-02-12 08:56:17 +0530

    Chnaged UI of individual bonds page

--- frontend/src/pages/IndivisualBonds/IndivisualBonds.jsx
+++ frontend/src/pages/IndivisualBonds/IndivisualBonds.jsx
@@ -73,28 +73,98 @@ export default function IndivisualBonds() {
         fetchData()
    },[units])
   return (
-    <div>
-      IndivisualBonds
-      <div>{bondType}</div>
-      <div>{chng}</div>
-      <div>{couponrate}</div>
-      <div>{creditrating}</div>
-      <div>{faceval}</div>
-      <div>{ltp}</div>
-      <div>{maturitydate}</div>
-      <div>{series}</div>
-      <div>{symbol}</div>
-      <div>{userTrade}</div>
-      <div>{value}</div>
-      <div>{volume}</div>
-      <div>{objid}</div>
-      <input
-        type="text"
-        placeholder="Enter the Units"
-        value={units}
-        onChange={(e)=>setUnits(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Invest</button>
+    <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg font-medium leading-6 text-gray-900">Bond Information</h3>
       </div>
+      <div className="border-t border-gray-200">
+        <dl>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Symbol</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{symbol}</dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Bond Type</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{bondType}</dd>
+          </div>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Coupon Rate</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{couponrate}</dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Chng(in %)</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{chng}</dd>
+          </div>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Credit Rating</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{creditrating}</dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Face Value:</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {faceval}
+            </dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">LTP</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {ltp}
+            </dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Maturity Date</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {maturitydate}
+            </dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Series</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {series}
+            </dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Value</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {value}
+            </dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Volume</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {volume}
+            </dd>
+          </div>
+          <div className = "flex items-center justify-center m-4">
+            <button
+              type="submit"
+              className="group relative  rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={handleSubmit}
+              >
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3 w-5">
+              </span>
+                Invest
+            </button>
+          </div>
+        </dl>
+      </div>
+    </div>
+
+    // <div>
+    //   <div></div>
+    //   <div>{}</div>
+    //   <div>{}</div>
+    //   <div>{userTrade}</div>
+    //   <div>{}</div>
+    //   <div>{}</div>
+    //   <div>{objid}</div>
+    //   <input
+    //     type="text"
+    //     placeholder="Enter the Units"
+    //     value={units}
+    //     onChange={(e)=>setUnits(e.target.value)}
+    //   />
+    //   <button >Invest</button>
+    //   </div>
   )
 }
