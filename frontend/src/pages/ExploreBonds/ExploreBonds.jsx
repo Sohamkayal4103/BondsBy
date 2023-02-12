78b67781edadf2d7bcde6c994548421a9f74c6af
SarveshLimaye <sarveshlimaye2002@gmail.com>
2023-02-12 07:46:07 +0530

    push notification done

--- frontend/src/pages/ExploreBonds/ExploreBonds.jsx
+++ frontend/src/pages/ExploreBonds/ExploreBonds.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import BondCard from "../../components/BondCard";
+import addNotification from 'react-push-notification';
 
 const ExploreBonds = () => {
   const [bonds, setBonds] = useState([]);
@@ -7,16 +8,29 @@ const ExploreBonds = () => {
   const getBonds = async () => {
     const response = await fetch(endpoint);
     const data = await response.json();
-    setBonds(data);
+    setBonds(data.slice(0,22));
   };
+  
+  const MaxLimit = 20;
+
 
   useEffect(() => {
+    if(bonds.length > MaxLimit){
+      addNotification({
+        title: 'New Bond Added',
+        subtitle: 'A new bond has been added to the list',
+        message: 'Trade now to get the best price',
+        theme: 'darkblue',
+        duration:6000,
+        native: true // when using native, your OS will handle theming.
+    });
+    }
     getBonds();
   }, []);
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r pt-6 pb-12">
       <div className="flex items-center justify-center flex-wrap gap-12">
-        {bonds.map((bond) => <BondCard {...bond} />).slice(0, 19)}
+        {bonds.map((bond) => <BondCard {...bond} />)}
       </div>
     </div>
   );
